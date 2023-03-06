d8455250ec57cc8c1df25b109d755befa5252f2a
sagar1621 <sagarbiradar1621@gmail.com>
2023-03-06 17:17:11 +0530

    changes to skills

--- pages/index.js
+++ pages/index.js
@@ -216,31 +216,9 @@ export default function Home() {
                 />
               </a>
             </div>
-            <div className="m-4">
-              <a
-                href="https://pandas.pydata.org/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src="https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/pandas/pandas-original.svg"
-                  alt="pandas"
-                  width="60"
-                  height="60"
-                />
-              </a>
-            </div>
+            
 
-            <div className="m-4">
-              <a href="https://www.python.org" target="_blank" rel="noreferrer">
-                <img
-                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
-                  alt="python"
-                  width="60"
-                  height="60"
-                />
-              </a>
-            </div>
+            
             <div className="m-4">
               <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
                 <img
@@ -261,20 +239,7 @@ export default function Home() {
                 />
               </a>
             </div>
-            <div className="m-4">
-              <a
-                href="https://scikit-learn.org/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg"
-                  alt="scikit_learn"
-                  width="60"
-                  height="60"
-                />
-              </a>
-            </div>
+           
             <div className="m-4">
               <a
                 href="https://tailwindcss.com/"
